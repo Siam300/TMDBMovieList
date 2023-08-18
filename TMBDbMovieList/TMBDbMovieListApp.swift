d89d94050b890d0b7684fdcf6a9aaf6c6d382b25
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-08-18 10:05:59 +0600

    Initial Commit

---
+++ TMBDbMovieList/TMBDbMovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMBDbMovieListApp.swift
+//  TMBDbMovieList
+//
+//  Created by Auto on 8/18/23.
+//
+
+import SwiftUI
+
+@main
+struct TMBDbMovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
