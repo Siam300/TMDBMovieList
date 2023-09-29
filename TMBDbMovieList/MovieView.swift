89b82e40158ab4a8375cb22275ed0e5099e00ed0
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-09-29 12:59:02 +0600

    Various bug fixed and design changed.

--- TMBDbMovieList/MovieView.swift
+++ TMBDbMovieList/MovieView.swift
@@ -7,6 +7,75 @@
 
 import SwiftUI
 
+struct MovieListView: View {
+    @ObservedObject var viewModel: MovieViewModel
+    @State private var selectedMovie: Movie?
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color("primary")
+                    .edgesIgnoringSafeArea(.all)
+                List {
+                    ForEach(viewModel.movies.indices, id: \.self) { index in
+                        let movie = viewModel.movies[index]
+                        Button(action: {
+                            selectedMovie = movie
+                        }) {
+                            HStack {
+                                if let posterPath = movie.poster_path,
+                                   let url = URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)") {
+                                    URLImage(url: url)
+                                        .frame(width: 150, height: 200)
+                                } else {
+                                    Image(systemName: "photo")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 150, height: 200)
+                                }
+                                LazyVStack(alignment: .leading) {
+                                    TextView(index: index, movie: movie)
+                                }
+                                .padding()
+                                .cornerRadius(10)
+                                .foregroundColor(.black)
+                            }
+                            //.background(Color("primary"))
+                            .edgesIgnoringSafeArea(.all)
+                        }
+                    }
+                    .listRowBackground(Color("primary"))
+                }
+                .padding(0)
+                .navigationBarTitle("Top 20 Movies")
+                .sheet(item: $selectedMovie) { movie in
+                    MovieDetailsView(viewModel: viewModel, movie: movie)
+                }
+            }
+        }
+        .listStyle(PlainListStyle())
+        //.background(Color("primary"))
+    }
+    
+    func TextView(index: Int, movie: Movie) -> some View {
+        VStack(alignment: .leading) {
+            Text("Ranking: \(index + 1).")
+                .foregroundColor(.black)
+                .fontWeight(.bold)
+                .padding(.trailing)
+            Text(movie.title)
+                .font(.headline)
+                .foregroundColor(.black)
+            Text(String(format: "Average Vote: %.1f/10", movie.vote_average))
+                .font(.subheadline)
+                .foregroundColor(.black)
+            Text("Release Date: \(movie.release_date)")
+                .font(.caption)
+                .foregroundColor(.black)
+        }
+    }
+}
+
 struct URLImage: View {
     @ObservedObject var imageLoader: ImageLoader
     let placeholder: Image
@@ -21,79 +90,12 @@ struct URLImage: View {
             Image(uiImage: uiImage)
                 .resizable()
                 .scaledToFit()
+                .cornerRadius(10)
         } else {
             placeholder
                 .resizable()
                 .scaledToFit()
         }
-    }
-}
-
-struct MovieListView: View {
-    @ObservedObject var viewModel: MovieViewModel
-    @State private var selectedMovie: Movie?
-    
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(viewModel.movies.indices, id: \.self) { index in
-                    let movie = viewModel.movies[index]
-                    Button(action: {
-                        selectedMovie = movie
-                    }) {
-                        HStack {
-                            if let posterPath = movie.poster_path,
-                               let url = URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)") {
-                                URLImage(url: url)
-                                    .frame(width: 150, height: 200)
-                            } else {
-                                Image(systemName: "photo")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 150, height: 200)
-                            }
-                            LazyVStack(alignment: .leading) {
-                                Text("Ranking: \(index + 1).")
-                                    .foregroundColor(.black)
-                                    .fontWeight(.bold)
-                                    .padding(.trailing, 5)
-                                Text(movie.title)
-                                    .font(.headline)
-                                    .foregroundColor(.black)
-                                Text(String(format: "Average Vote: %.1f/10", movie.vote_average))
-                                    .font(.subheadline)
-                                    .foregroundColor(.black)
-                                Text("Release Date: \(movie.release_date)")
-                                    .font(.caption)
-                                    .foregroundColor(.black)
-                            }
-                            .padding()
-                            .cornerRadius(10)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color.white, lineWidth: 2)
-                            )
-                            .foregroundColor(.white)
-                        }
-                    }
-                }
-            }
-            .navigationBarTitle("Top Movies")
-            .onAppear {
-                viewModel.getData()
-            }
-            .sheet(item: $selectedMovie) { movie in
-                MovieDetailsView(viewModel: viewModel, movie: movie)
-            }
-        }
-        .listStyle(PlainListStyle())
-        .background(Color.black)
-    }
-}
-
-struct MovieListView_Previews: PreviewProvider {
-    static var previews: some View {
-        MovieListView(viewModel: MovieViewModel())
     }
 }
 
@@ -103,84 +105,80 @@ struct MovieDetailsView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        VStack {
-            Spacer()
-            Text("Title: \(movie.title)")
-                .fontWeight(.bold)
-                .font(.title)
-                .multilineTextAlignment(.center)
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Language: \(movie.original_language)")
-                        .font(.headline)
-                    Text(String(format: "Rating: %.1f/10", movie.vote_average))
-                        .font(.headline)
-                    Text("Vote Count: \(movie.vote_count)")
-                        .font(.headline)
-                    Text(String(format: "Popularity: %.3f", movie.popularity))
-                        .font(.headline)
-                    Text("Release Date: \(movie.release_date)")
-                        .font(.headline)
+        ZStack {
+            Color("secondary")
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                Text("Title: \(movie.title)")
+                    .fontWeight(.bold)
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Language: \(movie.original_language)")
+                            .font(.headline)
+                        Text(String(format: "Rating: %.1f/10", movie.vote_average))
+                            .font(.headline)
+                        Text("Vote Count: \(movie.vote_count)")
+                            .font(.headline)
+                        Text(String(format: "Popularity: %.3f", movie.popularity))
+                            .font(.headline)
+                        Text("Release Date: \(movie.release_date)")
+                            .font(.headline)
+                    }
+                    Spacer()
+                    if let posterPath = movie.poster_path,
+                       let url = URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)") {
+                        URLImage(url: url)
+                            .scaledToFit()
+                            .frame(width: 120, height: 200)
+                    }
                 }
                 Spacer()
-                if let posterPath = movie.poster_path,
-                   let url = URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)") {
-                    URLImage(url: url)
-                    //.resizable()
-                        .scaledToFit()
-                        .frame(width: 150, height: 200)
+                ScrollView{
+                    Text("Overview: \(movie.overview)")
+                        .font(.headline)
+                    if let backdropPath = movie.backdrop_path,
+                       let url = URL(string: "https://image.tmdb.org/t/p/w500\(backdropPath)") {
+                        URLImage(url: url)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 200)
+                            .cornerRadius(10)
+                            .padding(.top, 10)
+                        
+                    } else {
+                        Image(systemName: "photo")
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 200)
+                            .cornerRadius(10)
+                            .padding(.top, 10)
+                    }
                 }
+                Spacer()
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                }, label: {
+                    Image(systemName: "xmark.square.fill")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.black)
+                })
             }
-            ScrollView{
-                Text("Overview: \(movie.overview)")
-                    .font(.subheadline)
-                if let backdropPath = movie.backdrop_path,
-                   let url = URL(string: "https://image.tmdb.org/t/p/w500\(backdropPath)") {
-                    URLImage(url: url)
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 200)
-                        .cornerRadius(10)
-                        .padding(.top, 10)
-                } else {
-                    Image(systemName: "photo")
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 200)
-                        .cornerRadius(10)
-                        .padding(.top, 10)
-                }
-            }
-            Button(action: {
-                presentationMode.wrappedValue.dismiss()
-            }, label: {
-                Image(systemName: "xmark.circle")
-                    .font(.largeTitle)
-                    .foregroundColor(Color.black)
-            })
+            //.background(Color.clear)
+            .foregroundColor(.black)
+            .cornerRadius(15)
+            .frame(maxWidth: .infinity)
+            //            .overlay(
+            //                RoundedRectangle(cornerRadius: 10)
+            //                    .stroke(Color.black, lineWidth: 2)
+            //            )
+            .padding(20)
         }
-        .padding(.vertical)
-        .background(Color.white)
-        .cornerRadius(10)
-        .padding(10)
-        .frame(maxWidth: .infinity)
-        .overlay(
-            RoundedRectangle(cornerRadius: 10)
-                .stroke(Color.black, lineWidth: 2)
-        )
-        .padding(20)
     }
 }
 
-struct FullScreenImageView: View {
-    let url: URL
-    
-    var body: some View {
-        if let data = try? Data(contentsOf: url), let uiImage = UIImage(data: data) {
-            Image(uiImage: uiImage)
-                .resizable()
-                .scaledToFit()
-            //.edgesIgnoringSafeArea(.all)
-        } else {
-            Text("Error loading image")
-        }
+struct MovieListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieListView(viewModel: MovieViewModel())
     }
 }
